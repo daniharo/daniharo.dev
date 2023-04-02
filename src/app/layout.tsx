c36cfca3ec75de922484fb52c62f72cfeb41fd01
Daniel Haro Contreras <daniel@appfluence.com>
2023-04-02 14:33:38 +0200

    Fix h-screen in iOS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="flex flex-col md:block">{children}</body>
+    <html lang="en" className="h-full">
+      <body className="flex h-full flex-col md:block">{children}</body>
     </html>
   );
 }
